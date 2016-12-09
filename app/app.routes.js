a88185efd2b964940304dcfb55987c3e2ebfbc36
Zoe Villanueva <zoektv@gmail.com>
2016-12-09 12:40:29 +0800

    restriced normal user access

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,33 +1,68 @@
 (function(){
 	'use strict'
+    
+    var routeProvider = null;
+
+    // thank you cliff.meyers
 	angular
 		.module('mainApp')
-		.config(function($routeProvider){
-			$routeProvider
-				.when('/venues',{
-					templateUrl: '/venue/venue.html',
-					controller: 'venueController',
-					controllerAs: 'venue'
-				})
-				.when('/events',{
-					templateUrl: '/event/event.html',
-					controller: 'eventController',
-					controllerAs: 'event'
-				})
-				.when('/users',{
-					templateUrl: '/user/user.html',
-					controller: 'userController',
-					controllerAs: 'user'
-				})
-				.when('/requests',{
-					templateUrl: '/request/request.html',
-					controller: 'requestController',
-					controllerAs: 'request'
-				})
-				.when('/profile',{
-					templateUrl: '/profile/profile.html',
-					controller: 'profileController',
-					controllerAs: 'profile'
-				});
-		});
+		.config(function($routeProvider) {
+            routeProvider = $routeProvider;
+		})
+        .run(function($http) {
+            $http
+                .get('/loggedIn')
+                .then(function(response) {
+                    $http
+                        .get('/users/'+response.data)
+                        .then(function(response) {
+                            console.log(response.data);
+                            if (response.data.accounttype === 'admin') {
+                                routeProvider
+                                    .when('/venues',{
+                                        templateUrl: '/venue/venue.html',
+                                        controller: 'venueController',
+                                        controllerAs: 'venue'
+                                    })
+                                    .when('/events',{
+                                        templateUrl: '/event/event.html',
+                                        controller: 'eventController',
+                                        controllerAs: 'event'
+                                    })
+                                    .when('/users',{
+                                        templateUrl: '/user/user.html',
+                                        controller: 'userController',
+                                        controllerAs: 'user'
+                                    })
+                                    .when('/requests',{
+                                        templateUrl: '/request/request.html',
+                                        controller: 'requestController',
+                                        controllerAs: 'request'
+                                    })
+                                    .when('/profile',{
+                                        templateUrl: '/profile/profile.html',
+                                        controller: 'profileController',
+                                        controllerAs: 'profile'
+                                    });
+                            } else {
+                                routeProvider
+                                    .when('/venues',{
+                                        templateUrl: '/venue/venue.html',
+                                        controller: 'venueController',
+                                        controllerAs: 'venue'
+                                    })
+                                    .when('/events',{
+                                        templateUrl: '/event/event.html',
+                                        controller: 'eventController',
+                                        controllerAs: 'event'
+                                    })
+                                    .when('/profile',{
+                                        templateUrl: '/profile/profile.html',
+                                        controller: 'profileController',
+                                        controllerAs: 'profile'
+                                    });
+                            }
+                        });
+                });
+        });
 })();
